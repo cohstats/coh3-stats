96a1e61d93273efc02529c2cfa8da6a863ff9307
Petr Vecera <vecera.petr@gmail.com>
2022-10-15 20:23:40 +0200

    Add example leaderboards.tsx

--- pages/leaderboards.tsx
+++ pages/leaderboards.tsx
@@ -1,13 +1,33 @@
 import React, { useEffect, useState } from "react";
 
 const Leaderboards: React.FC = () => {
-  const [data, setData] = useState("");
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setData("something");
+    setIsLoading(true);
+
+    (async () => {
+      try {
+        const response = await fetch(
+          `https://us-east4-coh2-ladders-prod.cloudfunctions.net/getCOHLaddersHttp?leaderBoardID=4&start=0`,
+        );
+        const finalData = await response.json();
+        console.log(finalData);
+        setData(finalData);
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
   }, []);
 
-  return <>{data}</>;
+  return (
+    <>
+      loading: {JSON.stringify(isLoading)} <br /> DATA: {JSON.stringify(data)}
+    </>
+  );
 };
 
 export default Leaderboards;
