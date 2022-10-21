7c2dfc6b219a8ae5833fdbec5f7df7a71fe851c6
Petr Vecera <petr.vecera@profiq.com>
2022-10-21 10:28:03 +0200

    Add rum

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { NotificationsProvider } from "@mantine/notifications";
 import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer/Footer";
 import { useLocalStorage } from "@mantine/hooks";
+import Script from "next/script";
 
 export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   const { Component, pageProps } = props;
@@ -27,6 +28,16 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="shortcut icon" href="/logo/favicon.ico" />
       </Head>
+      <Script
+        id="rum-lib"
+        src="https://rum.layer0.co/latest.js"
+        onLoad={() => {
+          // @ts-ignore
+          new Layer0.Metrics({
+            token: "43cf5623-832d-4e95-aae9-8c2a9368680c",
+          }).collect();
+        }}
+      />
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
         <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
