c73d4963e3939588c259218b69798d70c2ded706
Petr Vecera <vecera.petr@gmail.com>
2023-03-06 18:29:19 +0100

    Incerase search debounce

--- components/customSpotlightProvider.tsx
+++ components/customSpotlightProvider.tsx
@@ -13,7 +13,7 @@ const CustomSpotlightProvider = ({ children }: { children: ReactNode[] | null })
   const [actions, setActions] = useState<[] | SpotlightAction[]>([]);
   const [fetchLoading, setFetchLoading] = useState(false);
   // debounce query (wait till user stopped actively entering stuff in the search field)
-  const [debouncedQuery, setDebouncedQuery] = useDebouncedState("", 200);
+  const [debouncedQuery, setDebouncedQuery] = useDebouncedState("", 500);
 
   /**
    * Request additional results for the spotlight search based on the user input and add to spotlight entries
