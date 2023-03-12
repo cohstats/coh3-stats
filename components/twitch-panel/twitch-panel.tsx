271c2d421504bba6e88c7c0a271f7e8e5c4db4ae
Ryan Sullivan <mrsulliv@gmail.com>
2023-03-12 11:46:00 -0700

    remove check for window and document

--- components/twitch-panel/twitch-panel.tsx
+++ components/twitch-panel/twitch-panel.tsx
@@ -9,32 +9,30 @@ declare global {
 
 const TwitchPanel = () => {
   useEffect(() => {
-    if (window && document) {
-      // this gate only be needed because of react strict mode running things twice
-      if (document.getElementById("twitch-script") !== null) return;
+    // this gate only be needed because of react strict mode running things twice
+    if (document.getElementById("twitch-script") !== null) return;
 
-      const script = document.createElement("script");
-      script.src = "https://player.twitch.tv/js/embed/v1.js";
-      script.id = "twitch-script";
-      document.body.appendChild(script);
+    const script = document.createElement("script");
+    script.src = "https://player.twitch.tv/js/embed/v1.js";
+    script.id = "twitch-script";
+    document.body.appendChild(script);
 
-      script.addEventListener("load", () => {
-        const embed = new window.Twitch.Embed("twitch-embed", {
-          width: 854,
-          height: 480,
-          channel: "monstercat",
-          layout: "video",
-          autoplay: false,
-          // Only needed if this page is going to be embedded on other websites
-          parent: ["embed.example.com", "othersite.example.com"],
-        });
-
-        embed.addEventListener(window.Twitch.Embed.VIDEO_READY, () => {
-          const player = embed.getPlayer();
-          player.play();
-        });
+    script.addEventListener("load", () => {
+      const embed = new window.Twitch.Embed("twitch-embed", {
+        width: 854,
+        height: 480,
+        channel: "monstercat",
+        layout: "video",
+        autoplay: false,
+        // Only needed if this page is going to be embedded on other websites
+        parent: ["embed.example.com", "othersite.example.com"],
       });
-    }
+
+      embed.addEventListener(window.Twitch.Embed.VIDEO_READY, () => {
+        const player = embed.getPlayer();
+        player.play();
+      });
+    });
   }, []);
   return (
     <Paper shadow="xs" radius="md" mt="md" p="lg" color="gray">
