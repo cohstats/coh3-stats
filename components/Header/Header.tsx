96442b03a185d2fe725c3e68644a1ad227276fef
Petr Vecera <vecera.petr@gmail.com>
2023-03-12 13:29:43 +0100

    Tweaks on the header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,21 +4,16 @@ import {
   Container,
   Group,
   Header as MantineHeader,
-  Image as MantineImage,
   Title,
   Divider,
   Drawer,
   ScrollArea,
   HoverCard,
-  Text,
-  SimpleGrid,
   createStyles,
   Stack,
   ActionIcon,
   Tooltip,
   Anchor,
-  Grid,
-  Flex,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import Image from "next/image";
@@ -27,9 +22,9 @@ import { IconBarrierBlock, IconChevronDown } from "@tabler/icons";
 import { ColorSchemeToggle } from "../other/color-scheme-toggle";
 import { SearchButton } from "../search-button/search-button";
 import { OnlinePlayers } from "../online-players";
-import { raceType } from "../../src/coh3/coh3-types";
-import { localizedNames } from "../../src/coh3/coh3-data";
 import config from "../../config";
+import ExplorerMenu from "./components/ExplorerMenu";
+import LeaderboardsMenu from "./components/LeaderboardsMenu";
 
 export interface HeaderProps {
   // children?: React.ReactNode;
@@ -122,24 +117,82 @@ export const Header: React.FC<HeaderProps> = () => {
   const { classes, cx } = useStyles();
   const [opened, { toggle, close }] = useDisclosure(false);
 
-  const leaderboardFactionLink = (faction: raceType, gamemode: string) => (
-    <Text>
-      <Anchor component={Link} href={"/leaderboards?race=" + faction + "&type=" + gamemode}>
-        {localizedNames[faction]}
-      </Anchor>
-    </Text>
+  const MobileView = (
+    <>
+      <Burger opened={opened} onClick={toggle} className={classes.burger} size="sm" />
+      <Drawer
+        opened={opened}
+        onClose={close}
+        size="100%"
+        padding="md"
+        title="Navigation"
+        className={classes.hiddenDesktop}
+        zIndex={1000000}
+      >
+        <ScrollArea sx={{ height: "calc(100vh - 60px)" }} mx="-md">
+          <Divider my="sm" />
+          <Stack px="md">
+            <Group grow>
+              <SearchButton onClick={() => close()} />
+            </Group>
+            <Anchor
+              component={Link}
+              href="/leaderboards"
+              className={classes.link}
+              onClick={() => close()}
+            >
+              Leaderboards{" "}
+            </Anchor>
+            <Tooltip label="Coming soon" color="orange">
+              <Anchor
+                component={Link}
+                href="#"
+                className={cx(classes.link, classes.disabledLink)}
+                onClick={() => close()}
+              >
+                Statistics{" "}
+                <ActionIcon color="orange" size="sm" radius="xl" variant="transparent">
+                  <IconBarrierBlock size={16} />
+                </ActionIcon>
+              </Anchor>
+            </Tooltip>
+
+            <Tooltip label="Coming soon" color="orange">
+              <Anchor
+                component={Link}
+                href="#"
+                className={cx(classes.link, classes.disabledLink)}
+                onClick={() => close()}
+              >
+                App{" "}
+                <ActionIcon color="orange" size="sm" radius="xl" variant="transparent">
+                  <IconBarrierBlock size={16} />
+                </ActionIcon>
+              </Anchor>
+            </Tooltip>
+
+            {config.isDevEnv() ? <ExplorerMenu cx={cx} classes={classes} /> : <></>}
+
+            <Anchor
+              component={Link}
+              href="/about"
+              className={cx(classes.link)}
+              onClick={() => close()}
+            >
+              About{" "}
+            </Anchor>
+          </Stack>
+
+          <Divider my="sm" />
+
+          <Group px="md">
+            <ColorSchemeToggle onClick={() => close()} />
+          </Group>
+        </ScrollArea>
+      </Drawer>
+    </>
   );
 
-  const gamemodeLeaderboards = (gamemode: string) => (
-    <div>
-      <Text weight={700}>{gamemode}</Text>
-      <Divider my="sm" />
-      {leaderboardFactionLink("american", gamemode)}
-      {leaderboardFactionLink("german", gamemode)}
-      {leaderboardFactionLink("dak", gamemode)}
-      {leaderboardFactionLink("british", gamemode)}
-    </div>
-  );
   return (
     <>
       <MantineHeader height={60} className={classes.root}>
@@ -172,12 +225,7 @@ export const Header: React.FC<HeaderProps> = () => {
                 </div>
               </HoverCard.Target>
               <HoverCard.Dropdown sx={{ overflow: "hidden" }}>
-                <SimpleGrid cols={4} spacing={0}>
-                  {gamemodeLeaderboards("1v1")}
-                  {gamemodeLeaderboards("2v2")}
-                  {gamemodeLeaderboards("3v3")}
-                  {gamemodeLeaderboards("4v4")}
-                </SimpleGrid>
+                <LeaderboardsMenu />
               </HoverCard.Dropdown>
             </HoverCard>
             <Tooltip label="Coming soon" color="orange">
@@ -206,7 +254,7 @@ export const Header: React.FC<HeaderProps> = () => {
               </Anchor>
             </Tooltip>
 
-            {config.isDevEnv() ? explorerOption({ cx, classes }) : <></>}
+            {config.isDevEnv() ? <ExplorerMenu cx={cx} classes={classes} /> : <></>}
 
             <Anchor component={Link} href="/about" className={cx(classes.link)}>
               About{" "}
@@ -218,156 +266,9 @@ export const Header: React.FC<HeaderProps> = () => {
             <SearchButton />
             <ColorSchemeToggle />
           </Group>
-          <Burger opened={opened} onClick={toggle} className={classes.burger} size="sm" />
-          <Drawer
-            opened={opened}
-            onClose={close}
-            size="100%"
-            padding="md"
-            title="Navigation"
-            className={classes.hiddenDesktop}
-            zIndex={1000000}
-          >
-            <ScrollArea sx={{ height: "calc(100vh - 60px)" }} mx="-md">
-              <Divider my="sm" />
-              <Stack px="md">
-                <Group grow>
-                  <SearchButton onClick={() => close()} />
-                </Group>
-                <Anchor
-                  component={Link}
-                  href="/leaderboards"
-                  className={classes.link}
-                  onClick={() => close()}
-                >
-                  Leaderboards{" "}
-                </Anchor>
-                <Tooltip label="Coming soon" color="orange">
-                  <Anchor
-                    component={Link}
-                    href="#"
-                    className={cx(classes.link, classes.disabledLink)}
-                    onClick={() => close()}
-                  >
-                    Statistics{" "}
-                    <ActionIcon color="orange" size="sm" radius="xl" variant="transparent">
-                      <IconBarrierBlock size={16} />
-                    </ActionIcon>
-                  </Anchor>
-                </Tooltip>
-
-                <Tooltip label="Coming soon" color="orange">
-                  <Anchor
-                    component={Link}
-                    href="#"
-                    className={cx(classes.link, classes.disabledLink)}
-                    onClick={() => close()}
-                  >
-                    App{" "}
-                    <ActionIcon color="orange" size="sm" radius="xl" variant="transparent">
-                      <IconBarrierBlock size={16} />
-                    </ActionIcon>
-                  </Anchor>
-                </Tooltip>
-
-                {config.isDevEnv() ? explorerOption({ cx, classes }) : <></>}
-
-                <Anchor
-                  component={Link}
-                  href="/about"
-                  className={cx(classes.link)}
-                  onClick={() => close()}
-                >
-                  About{" "}
-                </Anchor>
-              </Stack>
-
-              <Divider my="sm" />
-
-              <Group px="md">
-                <ColorSchemeToggle onClick={() => close()} />
-              </Group>
-            </ScrollArea>
-          </Drawer>
+          {MobileView}
         </Container>
       </MantineHeader>
     </>
   );
 };
-
-/** @TODO Got lazy to replace dak for afrika_korps :D */
-const explorerFactionLink = (faction: raceType) => {
-  const iconFaction = faction === "dak" ? "afrika_korps" : faction;
-  return (
-    <Flex direction="row" align="center" gap="md">
-      <MantineImage
-        height={24}
-        width={24}
-        fit="contain"
-        src={`/icons/common/races/${iconFaction}.png`}
-        alt={localizedNames[faction]}
-      />
-      <Anchor color="orange" component={Link} href={`/explorer/races/${faction}`}>
-        {localizedNames[faction]}
-      </Anchor>
-    </Flex>
-  );
-};
-
-/**
- * @TODO Provide the toolName type for the routes. In the meantime, provide the
- * route fragment as string.
- */
-const explorerToolLink = (toolName: string, url: string) => (
-  <Text>
-    <Anchor color="orange" component={Link} href={`/explorer?tool=${url}`}>
-      {toolName}
-    </Anchor>
-  </Text>
-);
-
-const explorerOption = ({
-  cx,
-  classes,
-}: {
-  cx: (...args: any) => string;
-  classes: Record<string, string>;
-}) => (
-  <HoverCard width={800} position="bottom" radius="md" shadow="md" withinPortal>
-    <HoverCard.Target>
-      <Anchor href={config.isDevEnv() ? "/explorer" : "#"} className={cx(classes.link)}>
-        <Group spacing={3}>
-          Explorer
-          <IconChevronDown className={classes.hiddenMobile} size={16} />
-        </Group>
-      </Anchor>
-    </HoverCard.Target>
-    <HoverCard.Dropdown sx={{ overflow: "hidden" }}>
-      <Grid gutter={0} columns={2}>
-        <Grid.Col span={1}>
-          <Stack>
-            {explorerFactionLink("german")}
-            {explorerFactionLink("american")}
-            {explorerFactionLink("dak")}
-            {explorerFactionLink("british")}
-          </Stack>
-        </Grid.Col>
-        <Grid.Col span={1}>
-          <Stack>
-            <Text weight={700}>Other Stuff</Text>
-            <Text>
-              <Anchor color="orange" component={Link} href="#">
-                Child stuff
-              </Anchor>
-            </Text>
-          </Stack>
-          <Divider my="sm" />
-          <Stack>
-            <Text weight={700}>Tools</Text>
-            {explorerToolLink("DPS Calculator", "dps_calculator")}
-          </Stack>
-        </Grid.Col>
-      </Grid>
-    </HoverCard.Dropdown>
-  </HoverCard>
-);
