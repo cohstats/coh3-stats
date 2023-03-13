5607a21ece5ae835be9e6fe0125541147c1b59c1
Petr Vecera <vecera.petr@gmail.com>
2023-03-13 17:01:23 +0100

    Add twitch streams API

--- src/coh3/coh3-types.ts
+++ src/coh3/coh3-types.ts
@@ -132,3 +132,21 @@ export interface MatchHistory {
   profile_ids: number[];
   steam_ids: string[];
 }
+
+export interface TwitchStream {
+  id: string;
+  user_id: string;
+  user_login: string;
+  user_name: string;
+  game_id: string;
+  game_name: string;
+  type: string;
+  title: string;
+  viewer_count: number;
+  started_at: string;
+  language: string;
+  thumbnail_url: string;
+  tag_ids: string[];
+  tags: string[];
+  is_mature: boolean;
+}
