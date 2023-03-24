7030193639cef3c95aecf9b73f6777f237001d59
Ryan Sullivan <mrsulliv@gmail.com>
2023-03-23 19:10:43 -0700

    open bigger map recent matches page

--- components/matches-table/render-map.tsx
+++ components/matches-table/render-map.tsx
@@ -1,9 +1,11 @@
 import { maps } from "../../src/coh3/coh3-data";
-import { Text } from "@mantine/core";
+import { Modal, Text } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import Image from "next/image";
 import React from "react";
 
 const RenderMap = ({ mapName }: { mapName: string }) => {
+  const [opened, { open, close }] = useDisclosure(false);
   // In case we don't track the map, eg custom maps
   if (!maps[mapName]) {
     return (
@@ -16,12 +18,17 @@ const RenderMap = ({ mapName }: { mapName: string }) => {
   }
 
   return (
-    <div>
-      <Image src={maps[mapName]?.url} width={60} height={60} alt={mapName} loading="lazy" />
-      <Text align="center" style={{ whiteSpace: "nowrap" }}>
-        {maps[mapName]?.name}
-      </Text>
-    </div>
+    <>
+      <Modal opened={opened} onClose={close} title={maps[mapName].name}>
+        <Image src={maps[mapName]?.url} width={400} height={400} alt={mapName} loading="lazy" />
+      </Modal>
+      <div onClick={open} style={{ cursor: "pointer" }}>
+        <Image src={maps[mapName]?.url} width={60} height={60} alt={mapName} loading="lazy" />
+        <Text align="center" style={{ whiteSpace: "nowrap" }}>
+          {maps[mapName]?.name}
+        </Text>
+      </div>
+    </>
   );
 };
 
