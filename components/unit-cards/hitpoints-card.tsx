148a4e5d07018d2b63524f09f773297798b32ad3
KingDarBoja <stevenbojato04@gmail.com>
2023-03-25 09:23:17 -0500

    fix: split squad and gun hitpoints

--- components/unit-cards/hitpoints-card.tsx
+++ components/unit-cards/hitpoints-card.tsx
@@ -1,4 +1,4 @@
-import { Flex, Grid, Progress, Stack, Text, Title } from "@mantine/core";
+import { Flex, Progress, Stack, Text, Title, Tooltip } from "@mantine/core";
 import { EbpsType, SbpsType } from "../../src/unitStats";
 import ImageWithFallback, { iconPlaceholder } from "../placeholders";
 
@@ -8,47 +8,106 @@ type HitpointsCardInput = {
 };
 
 const squadIcon = "/icons/common/squad/squad.png";
+const weaponIcon = "/icons/races/common/symbols/hmg.png";
 const colorPalette = ["cyan.6", "blue.6", "indigo.6", "violet.6", "grape.6", "pink.6", "red.6"];
 
 export const HitpointCard = ({ squad, entities }: HitpointsCardInput) => {
-  const hitpoints: number[] = [];
+  // Put the "team_weapon" type in this array.
+  const twEntities: EbpsType[] = [];
+  // Only take into account the entities that are not a "team_weapon" type.
+  const squadEntities: EbpsType[] = [];
   for (const loadout of squad?.loadout || []) {
     const id = loadout.type.split("/").slice(-1)[0];
     const foundEntity = entities.find((x) => x.id === id);
     if (foundEntity) {
       // Push the entity hitpoints "num" times.
       for (let c = 0; c < loadout.num; c++) {
-        hitpoints.push(foundEntity.health.hitpoints);
+        if (foundEntity.unitTypes.includes("team_weapon")) {
+          twEntities.push(foundEntity);
+        } else {
+          squadEntities.push(foundEntity);
+        }
       }
     }
   }
 
-  const total = hitpoints.reduce((acc, x) => x + acc, 0);
+  const totalHitpoints = squadEntities.reduce((acc, x) => acc + x.health.hitpoints, 0);
+  const squadHitpoints = squadEntities.map((x) => x.health.hitpoints);
+  const twTotalHitpoints = twEntities.reduce((acc, x) => acc + x.health.hitpoints, 0);
+  const twSquadHitpoints = twEntities.map((x) => x.health.hitpoints);
+
+  // Display the first team weapon entity symbol as icon.
+  const twHitpointsSection = (
+    <Flex direction="row" align="center" gap="md">
+      <Stack align="center">
+        <ImageWithFallback
+          width={32}
+          height={32}
+          src={weaponIcon}
+          alt="squad team weapon icon"
+          fallbackSrc={iconPlaceholder}
+        />
+      </Stack>
+
+      <Progress
+        w="100%"
+        size={24}
+        sections={twSquadHitpoints.map((hp, idx) => ({
+          value: (hp / twTotalHitpoints) * 100,
+          color: colorPalette[idx],
+          label: hp.toString(),
+        }))}
+      />
+
+      <Text fw="bold" align="end">
+        {twTotalHitpoints}
+      </Text>
+    </Flex>
+  );
+
+  const squadHitpointsSection = (
+    <Flex direction="row" align="center" gap="md">
+      <Stack align="center">
+        <ImageWithFallback
+          width={32}
+          height={32}
+          src={squadIcon}
+          alt="squad icon"
+          fallbackSrc={iconPlaceholder}
+        />
+      </Stack>
+
+      <Progress
+        w="100%"
+        size={24}
+        sections={squadHitpoints.map((hp, idx) => ({
+          value: (hp / totalHitpoints) * 100,
+          color: colorPalette[idx],
+          label: hp.toString(),
+        }))}
+      />
+
+      <Text fw="bold" align="end">
+        {totalHitpoints}
+      </Text>
+    </Flex>
+  );
 
   return (
     <Stack>
       <Title order={6} transform="uppercase">
         Hitpoints
       </Title>
-      <Flex align="center" gap={16}>
-        <ImageWithFallback
-          width={32}
-          height={32}
-          src={squadIcon}
-          alt="squad entity icon"
-          fallbackSrc={iconPlaceholder}
-        />
-        <Progress
-          w="100%"
-          size={24}
-          sections={hitpoints.map((hp, idx) => ({
-            value: (hp / total) * 100,
-            color: colorPalette[idx],
-            label: hp.toString(),
-          }))}
-        />
-        <Text>{total}</Text>
-      </Flex>
+      <Tooltip label={"Squad hitpoints"} withArrow>
+        {squadHitpointsSection}
+      </Tooltip>
+      {twEntities.length ? (
+        <Tooltip label={"Team weapon hitpoints"} withArrow>
+          {twHitpointsSection}
+        </Tooltip>
+      ) : (
+        <></>
+      )}
     </Stack>
   );
 };
