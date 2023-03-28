fd8c82013c782e9b36c7517397321d02de2005a3
KingDarBoja <stevenbojato04@gmail.com>
2023-03-28 16:46:39 -0500

    refactor: no longer need the battlegroup icon mapping

--- components/unit-cards/battlegroup-card.tsx
+++ components/unit-cards/battlegroup-card.tsx
@@ -75,48 +75,6 @@ const BattlegroupBranchMapping = (branch: BattlegroupResolvedBranchType) => {
   );
 };
 
-/** Battlegroup icons are not 1:1 and the current ones make use of re-used assets. */
-const BattlegroupIcons = {
-  dak: {
-    armored_support: "races/afrika_corps/battlegroups/armored_ak_ger_portrait",
-    italian_combined_arms: "races/afrika_corps/battlegroups/combined_arms_ak_portrait",
-    italian_infantry: "races/afrika_corps/battlegroups/italian_infantry_ak_portrait",
-  } as Record<BattlegroupAfricaKorpsIds, string>,
-  german: {
-    breakthrough: "races/german/battlegroups/breakthrough_ger_portrait",
-    luftwaffe: "races/german/battlegroups/luftwaffe_ger_portrait",
-    mechanized: "races/german/battlegroups/mechanized_ger_ger_portrait",
-  } as Record<BattlegroupGermanIds, string>,
-  american: {
-    airborne: "races/american/battlegroups/paratroopers_us_portrait",
-    armored: "races/american/battlegroups/armored_us_portrait",
-    special_operations: "races/american/battlegroups/spec_ops_us_portrait",
-  } as Record<BattlegroupAmericanIds, string>,
-  british: {
-    british_air_and_sea: "races/british/battlegroups/air_and_sea_uk_portrait",
-    british_armored: "races/british/battlegroups/armored_uk_portrait",
-    indian_artillery: "races/british/battlegroups/indian_artillery_uk_portrait",
-  } as Record<BattlegroupBritishIds, string>,
-} as const;
-
-type BattlegroupAfricaKorpsIds = "armored_support" | "italian_combined_arms" | "italian_infantry";
-type BattlegroupGermanIds = "breakthrough" | "luftwaffe" | "mechanized";
-type BattlegroupBritishIds = "british_air_and_sea" | "british_armored" | "indian_artillery";
-type BattlegroupAmericanIds = "airborne" | "armored" | "special_operations";
-
-function getBattlegroupIcon(id: string, race: raceType) {
-  switch (race) {
-    case "dak":
-      return BattlegroupIcons.dak[id as BattlegroupAfricaKorpsIds];
-    case "german":
-      return BattlegroupIcons.german[id as BattlegroupGermanIds];
-    case "american":
-      return BattlegroupIcons.american[id as BattlegroupAmericanIds];
-    case "british":
-      return BattlegroupIcons.british[id as BattlegroupBritishIds];
-  }
-}
-
 export const BattlegroupCard = (
   race: raceType,
   data: {
@@ -144,7 +102,7 @@ export const BattlegroupCard = (
                 help_text: "",
                 extra_text: "",
                 brief_text: uiParent.briefText,
-                icon_name: getBattlegroupIcon(id, race),
+                icon_name: uiParent.iconName,
               }}
               time_cost={{
                 fuel: undefined,
