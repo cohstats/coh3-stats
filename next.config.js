a77b7bfe3a5cbe3d1aeaf8c0b2939bc0382a6e3c
Petr Vecera <vecera.petr@gmail.com>
2023-04-11 17:18:43 +0200

    Add sentry error tracing

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withSentryConfig } = require("@sentry/nextjs");
+
 // This file was automatically added by edgio init.
 // You should commit this file to source control.
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -13,18 +20,17 @@ const nextConfig = {
   },
 };
 
-const _preEdgioExport = nextConfig;
+const withEdgioConfig = withEdgio(
+  withServiceWorker({
+    // Output sourcemaps so that stack traces have original source filenames and line numbers when tailing
+    // the logs in the Edgio developer console.
+    edgioSourceMaps: true,
 
-module.exports = (phase, config) =>
-  withEdgio(
-    withServiceWorker({
-      // Output sourcemaps so that stack traces have original source filenames and line numbers when tailing
-      // the logs in the Edgio developer console.
-      edgioSourceMaps: true,
+    // Set the following to `true` to disable the Edgio dev tools.
+    disableEdgioDevTools: false,
 
-      // Set the following to `true` to disable the Edgio dev tools.
-      disableEdgioDevTools: false,
+    ...nextConfig,
+  }),
+);
 
-      ..._preEdgioExport,
-    }),
-  );
+module.exports = withSentryConfig(withEdgioConfig, { silent: true }, { hideSourcemaps: true });
