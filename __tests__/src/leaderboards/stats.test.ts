b7307f3e1059ffaf797e48b6062f8f4a889e705a
Petr Vecera <petr.vecera@profiq.com>
2023-04-13 00:43:57 +0200

    Remove the tests

--- __tests__/src/leaderboards/stats.test.ts
+++
@@ -1,7 +0,0 @@
-import { calculateLeaderboardStats } from "../../../src/leaderboards/stats";
-
-describe("calculateLeaderboardStats", () => {
-  it("should return the correct stats", async () => {
-    await calculateLeaderboardStats();
-  });
-});
