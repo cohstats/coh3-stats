b429ac95689ba2a645be7cb7590b3ac04c2732bf
Petr Vecera <vecera.petr@gmail.com>
2023-07-26 16:35:08 +0200

    Add webp images

--- components/faction-icon.tsx
+++ components/faction-icon.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 import { raceType } from "../src/coh3/coh3-types";
 // This is not something we want to do with all images
 // but these icons are used everywhere, so we can bake them into our app
-import americanIcon from "../public/icons/general/american.png";
-import germanIcon from "../public/icons/general/german.png";
-import britishIcon from "../public/icons/general/british.png";
-import dakIcon from "../public/icons/general/dak.png";
+import americanIcon from "../public/icons/general/american.webp";
+import germanIcon from "../public/icons/general/german.webp";
+import britishIcon from "../public/icons/general/british.webp";
+import dakIcon from "../public/icons/general/dak.webp";
 
 const FactionIcon = ({ name, width }: { name: raceType; width: number }) => {
   let icon = americanIcon;
