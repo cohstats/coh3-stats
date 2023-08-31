a27bfab6013d1e5be558ca006b5100e9d9d1cf2f
Mark Tallentire <mark.tallentire@hazcheck.com>
2023-08-31 22:16:40 +0100

    Complete

--- components/screens/home/twitch-panel/index.tsx
+++ components/screens/home/twitch-panel/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Flex, Grid, Title } from "@mantine/core";
+import { Box, Center, Container, Flex, Grid, Title, Text } from "@mantine/core";
 import { IconBrandTwitch } from "@tabler/icons-react";
 import { TwitchStream } from "../../../../src/coh3/coh3-types";
 import dynamic from "next/dynamic";
@@ -14,6 +14,39 @@ type Props = {
 };
 
 const TwitchContainer = ({ twitchStreams, error }: Props) => {
+  const renderPanel = () => {
+    if (!twitchStreams || twitchStreams.length === 0) {
+      return (
+        <Grid.Col md={12} sm={12} style={{ height: "100%" }}>
+          <Box
+            style={{
+              position: "relative",
+              width: "100%",
+              paddingTop: "28.1%",
+              paddingBottom: "28.1%",
+            }}
+          >
+            <Center>
+              <Text>No English speaking streams are currently broadcasting on Twitch.</Text>
+            </Center>
+          </Box>
+        </Grid.Col>
+      );
+    } else {
+      return (
+        <Grid.Col md={9} sm={12}>
+          {/*This is trick to have 16:9 aspect ratio but have 0 CLS*/}
+          <Box style={{ position: "relative", width: "100%", paddingTop: "56.25%" }}>
+            <div
+              id="twitch-embed"
+              style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
+            ></div>
+          </Box>
+        </Grid.Col>
+      );
+    }
+  };
+
   return (
     <Container size="fluid" px={"xs"} pb={5}>
       <Flex justify="flex-start" align="center" gap={5} pb="sm">
@@ -24,16 +57,7 @@ const TwitchContainer = ({ twitchStreams, error }: Props) => {
       </Flex>
 
       <Grid grow>
-        <Grid.Col md={9} sm={12}>
-          {/*This is trick to have 16:9 aspect ratio but have 0 CLS*/}
-          <Box style={{ position: "relative", width: "100%", paddingTop: "56.25%" }}>
-            <div
-              id="twitch-embed"
-              style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
-            ></div>
-          </Box>
-        </Grid.Col>
-
+        {renderPanel()}
         <TwitchPanel twitchStreams={twitchStreams} error={error} />
       </Grid>
     </Container>
