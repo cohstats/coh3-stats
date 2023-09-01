a18d4938d56e39797c70e00a6e175554a5298225
Mark Tallentire <mark.tallentire@hazcheck.com>
2023-09-01 14:39:41 +0100

    Extracted component out of index.tsx. Created new component for the leaderboard-section on the home page.

--- components/screens/home/index.tsx
+++ components/screens/home/index.tsx
@@ -14,11 +14,10 @@ import {
   Title,
 } from "@mantine/core";
 import { DPSCalculatorCard, UnitBrowserCard } from "./info-cards";
-import { IconTrophy } from "@tabler/icons-react";
 import React from "react";
 import TwitchContainer from "./twitch-panel";
 import Head from "next/head";
-import LeaderboardsTable from "../../leaderboards/leaderboards-table";
+import LeaderboardsSection from "./leaderboards-section/leaderboards-section";
 
 type Props = {
   twitchStreams: TwitchStream[] | null;
@@ -48,39 +47,7 @@ const Home: NextPage<Props> = ({ twitchStreams, error }) => {
           </Grid.Col>
         </Grid>
 
-        <Paper shadow="xs" radius="md" mt="md" p="lg" color="gray">
-          <Tabs variant="pills" defaultValue="british">
-            <Flex gap="md" justify="space-between" align="center" direction="row" wrap="wrap">
-              <Flex gap="xs" justify="flex-start" align="center" direction="row" wrap="wrap">
-                <IconTrophy></IconTrophy>{" "}
-                <Title order={1} size="h2">
-                  1v1 Leaderboards
-                </Title>
-              </Flex>
-              <Tabs.List>
-                <Tabs.Tab value="british">British Forces</Tabs.Tab>
-                <Tabs.Tab value="us">US Forces</Tabs.Tab>
-                <Tabs.Tab value="wehrmacht">Wehrmacht</Tabs.Tab>
-                <Tabs.Tab value="afrikakorps">Deutsches Afrikakorps</Tabs.Tab>
-              </Tabs.List>
-            </Flex>
-
-            <Tabs.Panel value="british" pt="xs">
-              <LeaderboardsTable withBorder={false} />
-            </Tabs.Panel>
-
-            <Tabs.Panel value="us" pt="xs">
-              Messages tab content
-            </Tabs.Panel>
-
-            <Tabs.Panel value="wehrmacht" pt="xs">
-              Settings tab content
-            </Tabs.Panel>
-            <Tabs.Panel value="afrikakorps" pt="xs">
-              Settings tab content
-            </Tabs.Panel>
-          </Tabs>
-        </Paper>
+        <LeaderboardsSection />
         <Paper shadow="xs" radius="md" mt="md" p="lg" color="gray" style={{ padding: 0 }}>
           <TwitchContainer twitchStreams={twitchStreams} error={error} />
         </Paper>
