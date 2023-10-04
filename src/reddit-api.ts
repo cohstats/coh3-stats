b44d132a31d0b3b877e4c56eaf3a6a57859b5638
Petr Vecera <petr.vecera@profiq.com>
2023-10-04 19:20:51 +0200

    Try catch reddit the shit

--- src/reddit-api.ts
+++ src/reddit-api.ts
@@ -1,3 +1,5 @@
+import { logger } from "./logger";
+
 interface RedditPostType {
   title: string;
   upvotes: number;
@@ -8,27 +10,32 @@ interface RedditPostType {
 }
 
 const getLatestCOH3RedditPosts = async (numberOfPosts = 10): Promise<RedditPostType[]> => {
-  const response = await fetch(
-    `https://www.reddit.com/r/CompanyOfHeroes/top.json?limit=100&t=month`,
-  );
-  const data = await response.json();
+  try {
+    const response = await fetch(
+      `https://www.reddit.com/r/CompanyOfHeroes/top.json?limit=100&t=month`,
+    );
+    const data = await response.json();
 
-  return data.data.children
-    .filter((post: any) => {
-      return `${post.data.link_flair_text}`.includes("CoH3");
-    })
-    .slice(0, numberOfPosts)
-    .map((post: any) => {
-      // we can't serialize undefined values
-      return {
-        title: post.data.title ?? null,
-        upvotes: post.data.ups ?? null,
-        author: post.data.author ?? null,
-        image: post.data.url_overridden_by_dest ?? null,
-        created: post.data.created_utc ?? null,
-        permalink: post.data.permalink ?? null,
-      };
-    });
+    return data.data.children
+      .filter((post: any) => {
+        return `${post.data.link_flair_text}`.includes("CoH3");
+      })
+      .slice(0, numberOfPosts)
+      .map((post: any) => {
+        // we can't serialize undefined values
+        return {
+          title: post.data.title ?? null,
+          upvotes: post.data.ups ?? null,
+          author: post.data.author ?? null,
+          image: post.data.url_overridden_by_dest ?? null,
+          created: post.data.created_utc ?? null,
+          permalink: post.data.permalink ?? null,
+        };
+      });
+  } catch (e) {
+    logger.error(e);
+    return [];
+  }
 };
 
 export type { RedditPostType };
