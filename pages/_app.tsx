ea8c15c7dbdc16858b5911c676a6feeccfc80b24
Petr Vecera <petr.vecera@profiq.com>
2023-10-14 12:21:51 +0200

    Add icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -117,11 +117,25 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
           content="Company of Heroes 3, COH3, COH3 Stats, COH3 Leaderboards, COH3 Player Cards, COH3 Info"
         />
         <link rel="shortcut icon" href="/logo/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/logo/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/logo/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/logo/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/logo/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/logo/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/logo/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/logo/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/logo/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/logo/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/logo/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/logo/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/logo/favicon-16x16.png" />
+        <link rel="icon" type="image/ico" sizes="16x16" href="/logo/favicon.ico" />
         <link
           type="application/opensearchdescription+xml"
           rel="search"
           title={"Search COH3 players"}
-          href="/opensearch.xml"
+          href="https://coh3stats.com/opensearch.xml"
         />
       </Head>
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
