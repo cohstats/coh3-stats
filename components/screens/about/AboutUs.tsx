ddbaf7d587181d54936c54222d84e7c8913fa7a4
Petr Vecera <vecera.petr@gmail.com>
2023-10-15 22:36:43 +0200

    Fix typo

--- components/screens/about/AboutUs.tsx
+++ components/screens/about/AboutUs.tsx
@@ -5,7 +5,7 @@ const AboutUs = () => {
     <>
       <Group>
         <Text pt="sm">
-          This website is a community-deiven project. We have most of our code open-source on our
+          This website is a community-driven project. We have most of our code open-source on our
           GitHub.
           <br />
           We are also on Discord, feel free to join us and contribute to the development or ask
