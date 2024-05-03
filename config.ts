83cf4076bc106e980dd85687ed9abefebe7572dc
Petr Vecera <vecera.petr@gmail.com>
2024-05-03 21:17:48 +0200

    Patch 1.6.6 update :)

--- config.ts
+++ config.ts
@@ -42,7 +42,7 @@ const getEdgioEnvName = (): string | null => {
 };
 
 // This controls the default patch selector in the stats page // this needs to be key statsPatchSelector object
-const defaultStatsPatchSelector = "1.6.1";
+const defaultStatsPatchSelector = "1.6.6";
 
 // This controls the patch selector in the stats page
 const statsPatchSelector: Record<
@@ -55,11 +55,11 @@ const statsPatchSelector: Record<
     group: string;
   }
 > = {
-  "1.6.5": {
+  "1.6.6": {
     from: "2024-05-02",
     to: "now",
     value: "1.6.5",
-    label: "1.6.5",
+    label: "1.6.5 - 1.6.6",
     group: "Coral Viper",
   },
   "1.6.1": {
@@ -159,11 +159,15 @@ const statsPatchSelector: Record<
 };
 
 // Latest patch needs to be a key to patches object
-const latestPatch = "1.6.5";
+const latestPatch = "1.6.6";
 
 // Get patchTimeSeconds here https://www.unixtimestamp.com/
 const patches: Record<string, { dataTag: string; dataTime: string; patchTimeSeconds?: number }> =
   {
+    "1.6.6": {
+      dataTag: "v1.6.6-1", // This is the tag of the data repo
+      dataTime: "03/May/2024", // The date when was the data tag created (the data extracted from game)
+    },
     "1.6.5": {
       dataTag: "v1.6.5-1", // This is the tag of the data repo
       dataTime: "01/May/2024", // The date when was the data tag created (the data extracted from game)
