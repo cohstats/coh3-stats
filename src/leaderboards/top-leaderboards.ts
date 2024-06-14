d0cd0cba2891a47eba97d8f7ab2eee10278c4456
Petr Vecera <vecera.petr@gmail.com>
2024-06-14 15:53:00 +0100

    Add history to the top 10 page (#499)

--- src/leaderboards/top-leaderboards.ts
+++ src/leaderboards/top-leaderboards.ts
@@ -1,10 +1,33 @@
 import { getLeaderBoardData } from "../coh3/coh3-api";
 import { findAndMergeStatGroups } from "../coh3/helpers";
 import { raceType, Top1v1LeaderboardsData } from "../coh3/coh3-types";
+import { getOldLeaderboardData } from "../apis/coh3stats-api";
 
 const getTop1v1LeaderBoards = async (race: raceType): Promise<Top1v1LeaderboardsData> => {
-  const leaderBoardDataRaw = await getLeaderBoardData(race, "1v1", 1, 10, 1, "steam");
-  const leaderBoardData = findAndMergeStatGroups(leaderBoardDataRaw, null);
+  const date = new Date();
+  const yesterdayTimeStamp =
+    Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() - 1, 0, 0, 0) / 1000;
+
+  const promisedLeaderBoardDataRaw = getLeaderBoardData(race, "1v1", 1, 10, 1, "steam");
+  const promisedOldData = getOldLeaderboardData(yesterdayTimeStamp, "1v1", race);
+
+  // Old data is already well formatted
+  const [leaderBoardDataRaw, oldData] = await Promise.all([
+    promisedLeaderBoardDataRaw,
+    promisedOldData,
+  ]);
+  // We need to convert the RAW data
+  let leaderBoardData = findAndMergeStatGroups(leaderBoardDataRaw, null);
+
+  // Add the change there
+  leaderBoardData = leaderBoardData.map((value) => {
+    if (oldData[value.statgroup_id]) {
+      value.change = value.rank - oldData[value.statgroup_id].rank;
+    } else {
+      value.change = "new";
+    }
+    return value;
+  });
 
   return {
     race: race,
