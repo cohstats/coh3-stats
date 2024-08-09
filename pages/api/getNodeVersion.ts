d057ebbe80433a438efa63cf7d345433d9e05e8c
Petr Vecera <vecera.petr@gmail.com>
2024-08-09 14:43:28 +0200

    Add more info to the getNodeVersion

--- pages/api/getNodeVersion.ts
+++ pages/api/getNodeVersion.ts
@@ -1,9 +1,27 @@
 import { logger } from "../../src/logger";
 import { NextApiRequest, NextApiResponse } from "next";
+import v8 from "v8";
+import os from "os";
+
+const systemInfo = {
+  platform: os.platform(), // Operating system platform
+  architecture: os.arch(), // CPU architecture
+  cpuCores: os.cpus(), // CPU information
+  totalMemory: (os.totalmem() / (1024 * 1024 * 1024)).toFixed(2) + " GB", // Total memory in GB
+  freeMemory: (os.freemem() / (1024 * 1024 * 1024)).toFixed(2) + " GB", // Free memory in GB
+  uptime: (os.uptime() / 3600).toFixed(2) + " hours", // System uptime in hours
+  homeDirectory: os.homedir(), // Home directory of the current user
+  hostname: os.hostname(), // Hostname of the operating system
+  networkInterfaces: os.networkInterfaces(), // Network interfaces
+};
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    res.setHeader("Cache-Control", "public").status(200).json({ nodeVersion: process.version });
+    res.setHeader("Cache-Control", "public").status(200).json({
+      nodeVersion: process.version,
+      heap: v8.getHeapStatistics(),
+      os: systemInfo,
+    });
   } catch (e) {
     logger.error(e);
     res.status(500).json({});
