f923ae1bb842df43898321c89a8fd7c80ab1a69a
Rajneesh Verma <rajneeshverma@Rajneesh-MacBook-Air.local>
2024-10-09 01:11:30 +0530

    remove the commanders and bulletins

--- components/PlayerMatchesDataTable.tsx
+++ components/PlayerMatchesDataTable.tsx
@@ -1,9 +1,7 @@
-"use client";
-
 import { useState } from "react";
 import { DataTable, DataTableColumn } from "mantine-datatable";
 import { PlayerReport } from "../src/coh3/coh3-types";
-import { Box, Flex, Text, Image, Group } from "@mantine/core";
+import { Text, Group } from "@mantine/core";
 import { IconBulb } from "@tabler/icons-react";
 
 const parseCounters = (counter: string, key: string) => {
@@ -94,18 +92,6 @@ const PlayerMatchesDataTable = ({ data, isWinner }: PlayerMatchesDataTableProps)
     },
   ];
 
-  const tableStyles = {
-    header: {
-      backgroundColor: "white",
-      color: "black",
-    },
-    table: {
-      "& tbody tr": {
-        borderBottom: "1px solid white",
-      },
-    },
-  };
-
   return (
     <>
       <Group align="center" mt="xs">
@@ -135,7 +121,6 @@ const PlayerMatchesDataTable = ({ data, isWinner }: PlayerMatchesDataTableProps)
               : [...currentIds, record.profile.alias],
           );
         }}
-        styles={tableStyles}
       />
     </>
   );
@@ -146,93 +131,7 @@ interface ExpandedRowContentProps {
 }
 
 const ExpandedRowContent = ({}: ExpandedRowContentProps) => {
-  const commanders = [
-    {
-      name: "Special Operations Doctrine",
-      image: "/images/commander-1.png",
-      items: [
-        { name: "Infiltration Grenades", image: "/images/commander-item-1.png" },
-        { name: "Sabotage", image: "/images/commander-item-1.png" },
-        { name: "Commando Raid", image: "/images/commander-item-1.png" },
-      ],
-    },
-    {
-      name: "Armored Assault Doctrine",
-      image: "/images/commander-1.png",
-      items: [
-        { name: "Panzer IV Command Tank", image: "/images/commander-item-1.png" },
-        { name: "Mechanized Assault Group", image: "/images/commander-item-1.png" },
-        { name: "Tiger Tank", image: "/images/commander-item-1.png" },
-      ],
-    },
-    {
-      name: "Luftwaffe Ground Forces",
-      image: "/images/commander-1.png",
-      items: [
-        { name: "Stuka Close Air Support", image: "/images/commander-item-1.png" },
-        { name: "88mm Flak 36", image: "/images/commander-item-1.png" },
-        { name: "Fallschirmjäger Drop", image: "/images/commander-item-1.png" },
-      ],
-    },
-  ];
-  const bulletins = [
-    {
-      name: "Recruit Training: Infantry",
-      description: "Riflemen and Volksgrenadiers have 3% increased accuracy",
-      image: "/images/bulletin-1.png",
-    },
-    {
-      name: "Officer Training: Infantry",
-      description: "Riflemen and Volksgrenadiers reload 3% faster.",
-      image: "/images/bulletin-1.png",
-    },
-    {
-      name: "Veteran Training: Infantry",
-      description:
-        "Riflemen and Volksgrenadiers rifles cooldown 2% faster between shots and reload 2% faster.",
-      image: "/images/bulletin-1.png",
-    },
-  ];
-
-  return (
-    <Flex p="md" gap={10}>
-      <div>
-        {commanders.map((commander, index) => (
-          <Box key={index} mb="sm">
-            <Flex gap={15}>
-              <Image w={80} h={80} src={commander.image} alt={commander.name} />
-              <Box>
-                <Text>{commander.name}</Text>
-                <Flex gap="xs" mt="xs">
-                  {commander.items.map((item, itemIndex) => (
-                    <Image
-                      key={itemIndex}
-                      src={item.image}
-                      alt={item.name}
-                      width={40}
-                      height={40}
-                    />
-                  ))}
-                </Flex>
-              </Box>
-            </Flex>
-          </Box>
-        ))}
-      </div>
-
-      <div>
-        {bulletins.map((bulletin, index) => (
-          <Flex gap={8} key={index} mb="xs">
-            <Image w={80} h={80} src={bulletin.image} alt={bulletin.name} />
-            <Box>
-              <Text>{bulletin.name}</Text>
-              <Text size="sm">{bulletin.description}</Text>
-            </Box>
-          </Flex>
-        ))}
-      </div>
-    </Flex>
-  );
+  return <></>;
 };
 
 export default PlayerMatchesDataTable;
