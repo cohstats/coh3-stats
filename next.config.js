6098d6bb8192546eb6152c48c3bc8cf6927ca482
Petr Vecera <vecera.petr@gmail.com>
2024-12-05 10:55:29 +0100

    Fix the path

--- next.config.js
+++ next.config.js
@@ -31,7 +31,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/*",
+        source: "/:path*",
         headers: [
           {
             key: "Netlify-Vary",
