783ec42b0f40fd7b22f8a9b65ab23a46886fb1ee
Petr Vecera <vecera.petr@gmail.com>
2024-12-13 23:58:42 +0100

    404

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,6 +4,7 @@ export default function Custom404() {
   return (
     <Center>
       <h1>404 - Page Not Found</h1>
+      <br />
       <h3>Please report on our Discord if you think this is a bug</h3>
     </Center>
   );
