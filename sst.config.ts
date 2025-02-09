19121aa0b13eaa880945ef5953f2e2726e344775
Petr Vecera <vecera.petr@gmail.com>
2025-02-09 18:13:22 +0100

    Do a deploy to AWS with SST

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
@@ -8,9 +7,38 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
+      providers: {
+        cloudflare: "5.49.0",
+        aws: {
+          region: "us-east-2",
+        },
+      },
     };
   },
   async run() {
-    new sst.aws.Nextjs("coh3-stats");
+    new sst.aws.Nextjs("coh3-stats", {
+      domain: {
+        name:
+          $app.stage === "staging"
+            ? "dev.coh3stats.com"
+            : $app.stage === "production"
+              ? "coh3stats.com"
+              : undefined,
+        dns: sst.cloudflare.dns({
+          proxy: true,
+          override: true,
+        }),
+      },
+      transform: {
+        cdn: (args) => {
+          args.transform = {
+            distribution: (distArgs) => {
+              // Only US and EU
+              distArgs.priceClass = "PriceClass_100";
+            },
+          };
+        },
+      },
+    });
   },
 });
