fae4940f6056349eef68c82e786ef39419fef932
Petr Vecera <vecera.petr@gmail.com>
2025-02-28 17:09:22 +0100

    Change md on the container

--- components/Content-container.tsx
+++ components/Content-container.tsx
@@ -7,7 +7,7 @@ interface ContentContainerProps {
 
 // Do we need any media queries here for responsiveness?
 const ContentContainer: React.FC<ContentContainerProps> = ({ children }) => (
-  <Container size={config.mainContainerSize} p={{ base: "xs", xs: "xs", sm: "xs", md: "md" }}>
+  <Container size={config.mainContainerSize} p={{ base: "xs", xs: "xs", sm: "xs", md: "xs" }}>
     {children}
   </Container>
 );
