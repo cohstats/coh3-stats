c314aa6a12480d8ec98ec914c9e06f7429b9b7a0
Petr Vecera <vecera.petr@gmail.com>
2025-04-06 13:14:29 +0200

    Add locales to the explorer

--- scripts/unit-paths.ts
+++ scripts/unit-paths.ts
@@ -1,4 +1,5 @@
 import { getMappings } from "../src/unitStats/mappings";
+import nextI18NextConfig from "../next-i18next.config";
 
 const pageHost = "https://coh3stats.com"
 
@@ -8,15 +9,22 @@ const generateAllUnitPages = async () => {
   const unitPaths = []
 
   const factions = ["american", "british", "german", "dak", "afrika_korps"];
+  const { locales } = nextI18NextConfig.i18n;
 
-  for (const faction of factions) {
-    const units = sbpsData.filter((squad: any) => squad.faction.includes(faction));
-    for(const unit of units){
-      const linkFaction = faction === "afrika_korps" ? "dak" : faction
+  for (const locale of locales) {
+    for (const faction of factions) {
+      const units = sbpsData.filter((squad: any) => squad.faction.includes(faction));
+      for(const unit of units){
+        const linkFaction = faction === "afrika_korps" ? "dak" : faction
 
-      unitPaths.push(`${pageHost}/explorer/races/${linkFaction}/units/${unit.id}`)
+        // For default locale (en), don't include locale in path
+        if (locale === nextI18NextConfig.i18n.defaultLocale) {
+          unitPaths.push(`${pageHost}/explorer/races/${linkFaction}/units/${unit.id}`)
+        } else {
+          unitPaths.push(`${pageHost}/${locale}/explorer/races/${linkFaction}/units/${unit.id}`)
+        }
+      }
     }
-
   }
 
   return unitPaths;
@@ -26,7 +34,6 @@ generateAllUnitPages().then((paths) => {
   for(const path of paths){
     console.log(path)
   }
-
 })
 
 export default generateAllUnitPages
