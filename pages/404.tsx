69e811724e4894dfdfc79cfb7c0772465bab7dfb
Petr Vecera <vecera.petr@gmail.com>
2025-05-07 10:07:31 +0200

    Fix 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,22 @@
 import { Stack } from "@mantine/core";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 export default function Custom404() {
+  const { t } = useTranslation("common");
+
   return (
     <Stack align="center" justify="center" style={{ minHeight: "20vh" }}>
-      <h1>404 - Page Not Found</h1>
-      <h3>Please report on our Discord if you think this is a bug</h3>
+      <h1>{t("errors.404.title")}</h1>
+      <h3>{t("errors.404.message")}</h3>
     </Stack>
   );
 }
+
+export const getStaticProps = async ({ locale = "en" }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
+};
