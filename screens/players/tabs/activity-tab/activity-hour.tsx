d12dc130a42188f19c6ca54b7f00e96b99c97fc8
Petr Vecera <vecera.petr@gmail.com>
2025-05-18 19:47:33 +0200

    Add timezone selector

--- screens/players/tabs/activity-tab/activity-hour.tsx
+++ screens/players/tabs/activity-tab/activity-hour.tsx
@@ -2,17 +2,61 @@ import { ProcessedCOHPlayerStats } from "../../../../src/coh3/coh3-types";
 import { useMantineColorScheme, Text, Group } from "@mantine/core";
 import { getNivoTooltipTheme } from "../../../../components/charts/charts-components-utils";
 import { ResponsiveBar } from "@nivo/bar";
-import React from "react";
+import React, { useMemo } from "react";
 import { calculateWinRate } from "../../../../src/utils";
 
-const ActivityByHour = ({ playerStatsData }: { playerStatsData: ProcessedCOHPlayerStats }) => {
+interface ActivityByHourProps {
+  playerStatsData: ProcessedCOHPlayerStats;
+  timezoneOffset?: number;
+}
+
+const ActivityByHour = ({ playerStatsData, timezoneOffset = 0 }: ActivityByHourProps) => {
   const { colorScheme } = useMantineColorScheme();
+
+  // Adjust the hour data based on the timezone offset
+  const adjustedHourData = useMemo(() => {
+    if (!playerStatsData.activityByHour || timezoneOffset === 0) {
+      return playerStatsData.activityByHour;
+    }
+
+    // Create a new array with adjusted hours
+    return playerStatsData.activityByHour
+      .map((hourData) => {
+        // Convert hour string to number
+        let hour = parseInt(hourData.hour);
+
+        // Apply timezone offset - handle fractional offsets
+        const integerOffset = Math.floor(timezoneOffset);
+        const fractionalOffset = timezoneOffset % 1;
+
+        // First apply the integer part of the offset
+        hour = (hour + integerOffset + 24) % 24;
+
+        // For fractional offsets, we need special handling
+        // For example, for a half-hour offset (0.5), we need to combine data from two adjacent hours
+        // This is a simplification - we're just shifting the hour without redistributing the data
+        if (fractionalOffset !== 0) {
+          // For positive fractional offsets, we round up
+          // For negative fractional offsets, we round down
+          if (fractionalOffset > 0) {
+            hour = (hour + 1) % 24;
+          }
+        }
+
+        // Return new object with adjusted hour
+        return {
+          ...hourData,
+          hour: hour.toString(),
+        };
+      })
+      .sort((a, b) => parseInt(a.hour) - parseInt(b.hour)); // Sort by hour to maintain order
+  }, [playerStatsData.activityByHour, timezoneOffset]);
 
   return (
     <ResponsiveBar
       margin={{ top: 10, right: 30, bottom: 40, left: 55 }}
       // @ts-ignore
-      data={playerStatsData.activityByHour as data[] | undefined}
+      data={adjustedHourData as data[] | undefined}
       layout={"vertical"}
       keys={["value"]}
       indexBy="hour"
--- screens/players/tabs/activity-tab/activity-tab.tsx
+++ screens/players/tabs/activity-tab/activity-tab.tsx
@@ -1,12 +1,24 @@
 import { ProcessedCOHPlayerStats } from "../../../../src/coh3/coh3-types";
-import { Container, Space, Title, Text, Center, Alert, Anchor } from "@mantine/core";
+import {
+  Container,
+  Space,
+  Title,
+  Text,
+  Center,
+  Alert,
+  Anchor,
+  Select,
+  Group,
+} from "@mantine/core";
 import dynamic from "next/dynamic";
 import dayjs from "dayjs";
 import { IconInfoTriangle } from "@tabler/icons-react";
 import Link from "next/link";
 import config from "../../../../config";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useTranslation } from "next-i18next";
+import HelperIcon from "../../../../components/icon/helper";
+import { isBrowserEnv } from "../../../../src/utils";
 
 // Because of some Nivo bugs we need to render only on client side
 const DynamicActivityByCalendarDay = dynamic(() => import("./activity-calendar-day"), {
@@ -25,6 +37,23 @@ const DynamicActivityByDayOfWeek = dynamic(() => import("./activity-week-day"), 
 const fromYear = "2023-01-02";
 const yearDiff = 1 + dayjs(new Date()).diff(fromYear, "year");
 
+/**
+ * Get the browser's timezone offset in hours
+ * @returns {number} Timezone offset in hours (e.g., -7 for PDT, 1 for CET)
+ */
+const getBrowserTimezoneOffset = (): number => {
+  if (!isBrowserEnv()) return 0;
+
+  // Get timezone offset in minutes and convert to hours
+  const offsetInMinutes = new Date().getTimezoneOffset();
+  // Note: getTimezoneOffset returns the difference between UTC and local time in minutes
+  // It returns negative values for timezones east of UTC and positive for west of UTC
+  // We need to invert the sign to match our GMT+/- format
+  const offset = -offsetInMinutes / 60;
+  // Round to the nearest integer for full hour timezones only
+  return Math.round(offset);
+};
+
 const ActivityTab = ({
   playerStatsData,
   platform,
@@ -33,6 +62,13 @@ const ActivityTab = ({
   platform: string;
 }) => {
   const { t } = useTranslation("players");
+  const [selectedTimezone, setSelectedTimezone] = useState<number>(0);
+
+  // Set the default timezone based on the browser's timezone on component mount
+  useEffect(() => {
+    const browserTimezone = getBrowserTimezoneOffset();
+    setSelectedTimezone(browserTimezone);
+  }, []);
 
   if (!playerStatsData) {
     return (
@@ -95,13 +131,61 @@ const ActivityTab = ({
       </div>
       <Space h={"lg"} />
       <div style={{ height: 200, paddingBottom: 50 }}>
-        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "flex-end" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "flex-end",
+            paddingRight: 30,
+          }}
+        >
           <Title order={2}>{t("activity.charts.hourOfDay.title")}</Title>
-          <Text span size="sm" fs="italic">
-            (GMT+00:00) UTC
-          </Text>
+          <Group gap="xs">
+            <HelperIcon text={t("activity.timezoneHelper")} iconSize={22} />
+            <Select
+              size="xs"
+              placeholder={t("activity.filters.timezone")}
+              value={selectedTimezone.toString()}
+              w={140}
+              onChange={(value) => setSelectedTimezone(Number(value))}
+              data={[
+                { value: "-12", label: "GMT-12:00" },
+                { value: "-11", label: "GMT-11:00" },
+                { value: "-10", label: "GMT-10:00" },
+                { value: "-9", label: "GMT-09:00" },
+                { value: "-8", label: "GMT-08:00" },
+                { value: "-7", label: "GMT-07:00" },
+                { value: "-6", label: "GMT-06:00" },
+                { value: "-5", label: "GMT-05:00" },
+                { value: "-4", label: "GMT-04:00" },
+                { value: "-3", label: "GMT-03:00" },
+                { value: "-2", label: "GMT-02:00" },
+                { value: "-1", label: "GMT-01:00" },
+                { value: "0", label: "GMT+00:00 (UTC)" },
+                { value: "1", label: "GMT+01:00" },
+                { value: "2", label: "GMT+02:00" },
+                { value: "3", label: "GMT+03:00" },
+                { value: "4", label: "GMT+04:00" },
+                { value: "5", label: "GMT+05:00" },
+                { value: "6", label: "GMT+06:00" },
+                { value: "7", label: "GMT+07:00" },
+                { value: "8", label: "GMT+08:00" },
+                { value: "9", label: "GMT+09:00" },
+                { value: "10", label: "GMT+10:00" },
+                { value: "11", label: "GMT+11:00" },
+                { value: "12", label: "GMT+12:00" },
+                { value: "13", label: "GMT+13:00" },
+                { value: "14", label: "GMT+14:00" },
+              ]}
+              allowDeselect={false}
+              withCheckIcon={false}
+            />
+          </Group>
         </div>
-        <DynamicActivityByHour playerStatsData={playerStatsData} />
+        <DynamicActivityByHour
+          playerStatsData={playerStatsData}
+          timezoneOffset={selectedTimezone}
+        />
       </div>
       <Space h={"lg"} />
       <div style={{ height: 200 }}>
