50acfb5614a2387b1f4d03a256ce5a047a211ea5
Petr Vecera <vecera.petr@gmail.com>
2025-06-25 11:00:33 +0200

    Try mantine help

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,16 @@
 import { ColorSchemeScript } from "@mantine/core";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
+// This is supposed to mimic mantineHtmlProps from @mantine/core
+const htmlProps = {
+  "data-mantine-color-scheme": "dark",
+  suppressHydrationWarning: true,
+};
+
 export default class _Document extends Document {
   render() {
     return (
-      <Html data-mantine-color-scheme="dark">
+      <Html {...htmlProps}>
         <Head>
           <ColorSchemeScript defaultColorScheme="dark" />
           {/* Add no-js fallback CSS with noscript tag */}
