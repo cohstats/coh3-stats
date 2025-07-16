b40734de4123bfa1a3d19fd238aa6a2690044c6c
Petr Vecera <vecera.petr@gmail.com>
2025-07-16 19:57:17 +0200

    Remove SST

--- .deprecated/sst.config.ts
+++ .deprecated/sst.config.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="./.sst/platform/config.d.ts" />
+/// <reference path="../.sst/platform/config.d.ts" />
 export default $config({
   app(input) {
     return {
