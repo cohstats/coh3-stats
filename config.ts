ba0497f3def9ee91ed4ecb8d9f49cdf526370f79
Petr Vecera <vecera.petr@gmail.com>
2025-07-16 20:49:27 +0200

    Config 2.1.2

--- config.ts
+++ config.ts
@@ -42,7 +42,7 @@ const getEdgioEnvName = (): string | null => {
 };
 
 // This controls the default patch selector in the stats page // this needs to be key statsPatchSelector object
-const defaultStatsPatchSelector = "2.1.1";
+const defaultStatsPatchSelector = "2.1.2";
 
 // This controls the patch selector in the stats page
 const statsPatchSelector: Record<
@@ -55,9 +55,16 @@ const statsPatchSelector: Record<
     group: string;
   }
 > = {
+  "2.1.2": {
+    from: "2025-07-16",
+    to: "now",
+    value: "2.1.2",
+    label: "2.1.2",
+    group: "Opal Scorpion",
+  },
   "2.1.1": {
     from: "2025-06-25",
-    to: "now",
+    to: "2025-07-16",
     value: "2.1.1",
     label: "2.1.0 - 2.1.1",
     group: "Opal Scorpion",
@@ -205,11 +212,15 @@ const statsPatchSelector: Record<
 };
 
 // Latest patch needs to be a key to patches object
-const latestPatch = "2.1.1";
+const latestPatch = "2.1.2";
 
 // Get patchTimeSeconds here https://www.unixtimestamp.com/
 const patches: Record<string, { dataTag: string; dataTime: string; patchTimeSeconds?: number }> =
   {
+    "2.1.2": {
+      dataTag: "v2.1.2-1",
+      dataTime: "16/July/2025",
+    },
     "2.1.1": {
       dataTag: "v2.1.1-1",
       dataTime: "27/June/2025",
