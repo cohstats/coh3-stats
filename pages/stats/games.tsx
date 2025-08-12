1cfc4055cbc0ba10dab5545835c363ece4c9c869
Petr Vecera <vecera.petr@gmail.com>
2025-08-12 15:28:05 +0200

    Add more translations

--- pages/stats/games.tsx
+++ pages/stats/games.tsx
@@ -4,7 +4,7 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 export const getStaticProps = async ({ locale = "en" }) => {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ["common"])),
+      ...(await serverSideTranslations(locale, ["common", "stats"])),
     },
   };
 };
