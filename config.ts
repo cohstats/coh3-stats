4382d8c2c58b7d06427a06a6bc4fbc30c89f9e60
Petr Vecera <vecera.petr@gmail.com>
2026-04-02 21:11:31 +0200

    Update to patch 2.3.1

--- config.ts
+++ config.ts
@@ -30,7 +30,7 @@ const getEdgioEnvName = (): string | null => {
 };
 
 // This controls the default patch selector in the stats page // this needs to be key statsPatchSelector object
-const defaultStatsPatchSelector = "2.3.0";
+const defaultStatsPatchSelector = "2.3.1";
 
 // This controls the patch selector in the stats page
 const statsPatchSelector: Record<
@@ -43,11 +43,11 @@ const statsPatchSelector: Record<
     group: string;
   }
 > = {
-  "2.3.0": {
+  "2.3.1": {
     from: "2026-04-01",
     to: "now",
-    value: "2.3.0",
-    label: "2.3.0",
+    value: "2.3.1",
+    label: "2.3.0 - 2.3.1",
     group: "Topaz Rhino",
   },
   "2.2.5": {
@@ -235,11 +235,15 @@ const statsPatchSelector: Record<
 };
 
 // Latest patch needs to be a key to patches object
-const latestPatch = "2.3.0";
+const latestPatch = "2.3.1";
 
 // Get patchTimeSeconds here https://www.unixtimestamp.com/
 const patches: Record<string, { dataTag: string; dataTime: string; patchTimeSeconds?: number }> =
   {
+    "2.3.1": {
+      dataTag: "v2.3.1-1",
+      dataTime: "01/Apr/2026",
+    },
     "2.3.0": {
       dataTag: "v2.3.0-1",
       dataTime: "01/Apr/2026",
