95d453d5d2ef85ee99d1a638c427b5ed40006eb5
Petr Vecera <vecera.petr@gmail.com>
2026-08-07 23:21:41 +0200

    2.5.3

--- config.ts
+++ config.ts
@@ -30,7 +30,7 @@ const getEdgioEnvName = (): string | null => {
 };
 
 // This controls the default patch selector in the stats page // this needs to be key statsPatchSelector object
-const defaultStatsPatchSelector = "2.5.2";
+const defaultStatsPatchSelector = "2.5.3";
 
 // This controls the patch selector in the stats page
 const statsPatchSelector: Record<
@@ -43,11 +43,11 @@ const statsPatchSelector: Record<
     group: string;
   }
 > = {
-  "2.5.2": {
+  "2.5.3": {
     from: "2026-07-29",
     to: "now",
-    value: "2.5.2",
-    label: "2.5.0 - 2.5.2",
+    value: "2.5.3",
+    label: "2.5.0 - 2.5.3",
     group: "2.5",
   },
   "2.4.2": {
@@ -249,13 +249,13 @@ const statsPatchSelector: Record<
 };
 
 // Latest patch needs to be a key to patches object
-const latestPatch = "2.5.2";
+const latestPatch = "2.5.3";
 
 // Get patchTimeSeconds here https://www.unixtimestamp.com/
 const patches: Record<string, { dataTag: string; dataTime: string; patchTimeSeconds?: number }> =
   {
-    "2.5.2": {
-      dataTag: "v2.5.2-3",
+    "2.5.3": {
+      dataTag: "v2.5.3-1",
       dataTime: "31/July/2026",
     },
     "2.4.2": {
